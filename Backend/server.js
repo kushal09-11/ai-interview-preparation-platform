f213b641c351f414b9baee4a3dc9394c2e7313af
kushal09-11 <ugs24117_it.saikushal@cbit.org.in>
2026-07-18 23:27:46 +0530

    Prepare backend for Render deployment

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,13 @@ const { generateInterviewReport } = require("./src/services/ai.service")
 connectToDB()
 generateInterviewReport({ resume, selfDescription, jobDescription })
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000");
-})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// app.listen(3000, ()=>{
+//     console.log("Server is running on port 3000");
+// })
