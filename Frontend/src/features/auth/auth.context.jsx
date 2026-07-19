662b3e7c8e692086ce74a8fc9b144f658f5c93a9
kushal09-11 <ugs24117_it.saikushal@cbit.org.in>
2026-07-19 19:52:35 +0530

    Add Vercel routing configuration

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,16 +1,63 @@
-import { createContext, useState} from "react";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../auth.context";
+import{ login, register, logout, getMe }  from "../services/auth.api";
 
-export const AuthContext = createContext()
+export const useAuth = () => {
 
-export const AuthProvider = ({ children }) => {
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
 
-    const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(true)
+    const handleLogin = async ({ email, password}) => {
+        setLoading(true)
+        try{
+            const data = await login({ email, password })
+            setUser(data.user)
+        }catch(err){
 
+        }finally{
+            setLoading(false)
+        }
+    }
 
-    return (
-        <AuthContext.Provider value={{user, setUser, loading, setLoading}} >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    const handleRegister = async ({ username, email, password}) => {
+        setLoading(true)
+        try{
+            const data = await register({ username, email, password })
+            setUser(data.user)
+        }catch(err){
+
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        try{
+            const data = await logout()
+            setUser(null)
+        }catch(err){
+
+        }finally{
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        
+        const getAndSetUser = async () => {
+            try{
+                const data = await getMe()
+                setUser(data.user)
+            }catch(err){
+
+            }finally{
+                setLoading(false)
+            }
+        }
+        getAndSetUser()
+        
+    }, [])
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
+}   
