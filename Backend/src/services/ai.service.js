bc6a4ef867ccb69a697d13137f9122302d301fdc
kushal09-11 <ugs24117_it.saikushal@cbit.org.in>
2026-07-22 13:47:12 +0530

    Fix Puppeteer Chromium compatibility

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,7 +1,9 @@
 const { GoogleGenAI } = require("@google/genai")
 const { z } = require("zod")
 // const { zodToJsonSchema } = require("zod-to-json-schema")
-const puppeteer = require("puppeteer")
+// const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
@@ -129,34 +131,61 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
     // };  
 }
 
-
 async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-        headless: true,
-        args: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox"
-        ]
-    })
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+    });
 
-    const page = await browser.newPage()
-     
-    await page.setContent(htmlContent, { waitUntil: 'networkidle0' })
+    const page = await browser.newPage();
 
-    const pdfBuffer = await page.pdf({ 
-        format: 'A4', margin: {
-            top: '20mm',
-            bottom: '20mm',
-            left: '15mm',
-            right: '15mm'
-        }
-    } )
+    await page.setContent(htmlContent, {
+        waitUntil: "networkidle0",
+    });
 
-    await browser.close()
+    const pdfBuffer = await page.pdf({
+        format: "A4",
+        margin: {
+            top: "20mm",
+            bottom: "20mm",
+            left: "15mm",
+            right: "15mm",
+        },
+    });
 
-    return pdfBuffer
+    await browser.close();
+
+    return pdfBuffer;
 }
+// async function generatePdfFromHtml(htmlContent) {
+//     const browser = await puppeteer.launch({
+//         executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+//         headless: true,
+//         args: [
+//             "--no-sandbox",
+//             "--disable-setuid-sandbox"
+//         ]
+//     })
+
+//     const page = await browser.newPage()
+     
+//     await page.setContent(htmlContent, { waitUntil: 'networkidle0' })
+
+//     const pdfBuffer = await page.pdf({ 
+//         format: 'A4', margin: {
+//             top: '20mm',
+//             bottom: '20mm',
+//             left: '15mm',
+//             right: '15mm'
+//         }
+//     } )
+
+//     await browser.close()
+
+//     return pdfBuffer
+// }
 
 async function generateResumePdf({resume, selfDescription, jobDescription}) {
 
